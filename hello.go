fd959235ac6cabb3b9d73b1eb150cd8f59997d8f
zhufengxiang <zhufengxiang@bytedance.com>
2019-09-03 11:32:50 +0800

    introduce hello with my first go library

--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/zzp8164/stringutil"
 )
 
 func main() {
 	fmt.Println("Hello, world. It is Tuesday today !")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
 }
 
